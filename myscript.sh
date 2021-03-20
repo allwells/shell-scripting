7078a116e0626b2149a31ba46e2ee525e2f0f99b
allwells <aleenfestus@gmail.com>
2021-03-20 17:49:27 +0100

    Comparison in shell ;)

--- myscript.sh
+++ myscript.sh
@@ -52,6 +52,13 @@
 # val1 -ge val2 Returns true if val1 is greater than or equal to val2
 # val1 -lt val2 Returns true if val1 is less than val2
 # val1 -le val2 Returns true if val1 is less than or equal to val2
+# NUM1=4
+# NUM2=2
 
-NUM1=4
-NUM2=2
+# if [ "$NUM1" -gt "$NUM2" ]
+# then
+#     echo "$NUM1 is greater than $NUM2"
+# else
+#     echo "$NUM1 is less than $NUM2"
+# fi
+
