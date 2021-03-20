19f6e2c55ce2998dedf9203578924095eb14364f
allwells <aleenfestus@gmail.com>
2021-03-20 18:02:02 +0100

    File conditions

--- myscript.sh
+++ myscript.sh
@@ -46,12 +46,14 @@
 
 
 # COMPARISON
+############
 # val1 -eq val2 Returns true if the values are equal
 # val1 -ne val2 Returns true if the values are not equal
 # val1 -gt val2 Returns true if val1 is greater than val2
 # val1 -ge val2 Returns true if val1 is greater than or equal to val2
 # val1 -lt val2 Returns true if val1 is less than val2
 # val1 -le val2 Returns true if val1 is less than or equal to val2
+############
 # NUM1=4
 # NUM2=2
 
@@ -62,3 +64,22 @@
 #     echo "$NUM1 is less than $NUM2"
 # fi
 
+# FILE CONDITIONS
+#################
+# -d file   True if the file is a directory
+# -e file   True if the file exists (note that this is not particularly portable, thus -f is generally used)
+# -f file   True if the provided string is a file
+# -g file   True if the group id is set on a file
+# -r file   True if the file is readable
+# -s file   True if the file has non-zero size
+# -u        True if the usr id is set on a profile
+# -w        True if the file is writable
+# -x        True if the file is executable
+#################
+# FILE="file.txt"
+# if [ -f "$FILE" ]
+# then
+#     echo "$FILE is a file."
+# else
+#     echo "$FILE is not a file."
+# fi
