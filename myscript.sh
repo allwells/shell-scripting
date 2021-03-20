0bd085c1c45a8e4c1b442e162d9122cc396e9ff7
allwells <aleenfestus@gmail.com>
2021-03-20 17:38:59 +0100

    Shell scripting is bae; F**k you Francisca!

---
+++ myscript.sh
@@ -0,0 +1,57 @@
+#! /usr/bin/bash
+
+
+# ECHO COMMAND
+# echo Hello Shell!
+
+
+# VARIABLES
+# UPPERCASE by convention
+# Letters, numbers and underscores
+# NAME="Allwell"
+# echo "My name is $NAME"
+
+
+# GET USER INPUT
+# read -p "Enter your name: " NAME
+# echo "Hello $NAME, you'll do great in shell scripting."
+
+
+# IF STATEMENT
+# if [ "$NAME" == "Allwell" ]
+# then
+#     echo "Hi $NAME, welcome back brother man."
+# fi
+
+
+# IF-ELSE STATEMENT
+# if [ "$NAME" == "Allwell" ]
+# then
+#     echo "Hi $NAME, welcome back brother man."
+# else
+#     echo "Who the f**k are you?"
+# fi
+
+
+# IF-ELSE-IF (elif) STATEMENT
+# if [ "$NAME" == "Allwell" ]
+# then
+#     echo "Wassup $NAME, welcome back brother man."
+# elif [ "$NAME" == "allwell" ]
+# then
+#     echo "Wassup $NAME, welcome back brother man."
+# else
+#     echo "Who the f**k is you??"
+# fi
+
+
+# COMPARISON
+# val1 -eq val2 Returns true if the values are equal
+# val1 -ne val2 Returns true if the values are not equal
+# val1 -gt val2 Returns true if val1 is greater than val2
+# val1 -ge val2 Returns true if val1 is greater than or equal to val2
+# val1 -lt val2 Returns true if val1 is less than val2
+# val1 -le val2 Returns true if val1 is less than or equal to val2
+
+NUM1=4
+NUM2=2
